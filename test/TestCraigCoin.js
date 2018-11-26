902dc97089144ba4828983a30b8fbdef32b295b9
CraigLawrence <craig@binary.com>
2018-11-26 21:00:23 +0100

    fixed test (not call for non-static method, one that creates transaction)

--- test/TestCraigCoin.js
+++ test/TestCraigCoin.js
@@ -5,7 +5,7 @@ contract('CraigCoin', function(accounts) {
         return CraigCoin.deployed().then(function(instance) {
             return instance.balanceOf.call(accounts[0]);
         }).then(function(balance) {
-            assert.equal(balance.valueOf(), 5000, "not 5000");
+            assert.equal(balance.toNumber(), 5000, "not 5000");
         });
     });
 
@@ -22,17 +22,16 @@ contract('CraigCoin', function(accounts) {
             return coin.balanceOf.call(accounts[1]);
         }).then(function(balance) {
             bBefore = balance.toNumber();
-            return coin.transfer.call(accounts[1], amount);
-        }).then(function(success) {
-            assert.isTrue(success);
+            return coin.transfer(accounts[1], amount);
+        }).then(function() {
             return coin.balanceOf.call(accounts[0]);
         }).then(function(balance) {
             aAfter = balance.toNumber();
             return coin.balanceOf.call(accounts[1]);
         }).then(function(balance) {
             bAfter = balance.toNumber();
-            assert.equal(aAfter, aBefore-amount, "sender balance not right");
-            assert.equal(bAfter, bBefore+amount, "receiever balance not right");
+            assert.equal(aAfter, aBefore - amount, "sender balance not right");
+            assert.equal(bAfter, bBefore + amount, "receiever balance not right");
         });
     });
 
